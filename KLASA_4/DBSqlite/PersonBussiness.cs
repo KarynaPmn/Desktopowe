7582fd5211fec5e7cf3daf0fed56a4106f1ad97f
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-10-28 09:01:25 +0100

    Add files via upload

---
+++ KLASA_4/DBSqlite/PersonBussiness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class PersonBussiness
+    {
+        PersonContext Context;
+        public PersonBussiness()
+        {
+            Context = new PersonContext();
+        }
+
+        public List<Person> getPersons() 
+        { 
+            List<Person> persons = new List<Person>();
+            var query = this.Context.Osoby;
+            foreach (var person in query)
+            {
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+    }
+}
---
+++ KLASA_4/DBSqlite/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class PersonContext : DbContext
+    {
+        public DbSet<Person> Osoby { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=../../../kurczak.db;");
+        }
+    }
+}
